3c14a059526bcfaee426ce091d78a5c31c880880
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2024-04-04 10:58:33 +0530

    done with the link in the profile

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -4,10 +4,13 @@ import FollowersModal from "../FollowersModal/FollowersModal";
 import { getAllUser } from "../../api/UserRequests";
 import User from "../User/User";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+
 const FollowersCard = ({ location }) => {
   const [modalOpened, setModalOpened] = useState(false);
   const [persons, setPersons] = useState([]);
   const { user } = useSelector((state) => state.authReducer.authData);
+  const navigate = useNavigate(); // Initialize useNavigate hook
 
   useEffect(() => {
     const fetchPersons = async () => {
@@ -17,12 +20,31 @@ const FollowersCard = ({ location }) => {
     fetchPersons();
   }, []);
 
+  const handleProfileClick = (userId) => {
+    console.log("hi")
+    navigate(`/profile/${userId}`); // Use navigate to navigate to user profile
+  };
+
   return (
-    <div className="FollowersCard"  >
-      <h3>Recomemded Users : </h3>
+    <div className="FollowersCard">
+      <h3>Recommended Users:</h3>
 
       {persons.map((person, id) => {
-        if (person._id !== user._id) return <User person={person} key={id} />;
+        console.log("hi") ; 
+        if (person._id !== user._id) {
+          console.log(person._id)
+          return (
+            
+            <User
+              style={{ cursor: "pointer" }} // Apply cursor pointer style
+              person={person}
+              key={id}
+
+              onClick={() => handleProfileClick(person._id)} // Add onClick event
+            />
+          );
+        }
+        return null;
       })}
       {!location ? (
         <span onClick={() => setModalOpened(true)}>Show more</span>
@@ -30,7 +52,7 @@ const FollowersCard = ({ location }) => {
         ""
       )}
 
-      <FollowersModal 
+      <FollowersModal
         modalOpened={modalOpened}
         setModalOpened={setModalOpened}
       />
--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { followUser, unfollowUser } from "../../actions/UserAction";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+
 const User = ({ person }) => {
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useSelector((state) => state.authReducer.authData);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   
-  const [following, setFollowing] = useState(
-    person.followers.includes(user._id)
-  );
+  const [following, setFollowing] = useState(person.followers.includes(user._id));
+
   const handleFollow = () => {
     following
       ? dispatch(unfollowUser(person._id, user))
       : dispatch(followUser(person._id, user));
     setFollowing((prev) => !prev);
   };
+
   return (
     <div className="follower">
       <div>
-        <img
-          src={
-            publicFolder + person.profilePicture
-              ? publicFolder + person.profilePicture
-              : publicFolder + "defaultProfile.png"
-          }
-          alt="profile"
-          className="followerImage"
-        />
+        {/* Wrap the image inside a Link component */}
+        <Link to={`/profile/${person._id}`}>
+          <img
+            src={
+              publicFolder + (person.profilePicture ? person.profilePicture : "defaultProfile.png")
+            }
+            alt="profile"
+            className="followerImage"
+          />
+        </Link>
         <div className="name">
           <span>{person.firstname}</span>
           <span>@{person.username}</span>
         </div>
       </div>
       <button
-        className={
-          following ? "button fc-button UnfollowButton" : "button fc-button"
-        }
+        className={following ? "button fc-button UnfollowButton" : "button fc-button"}
         onClick={handleFollow}
       >
         {following ? "Unfollow" : "Follow"}
